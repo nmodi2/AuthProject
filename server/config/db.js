7749c6f5c9d75661a6fefad853e509c65c35fd1a
nmodi2 <79933465+nmodi2@users.noreply.github.com>
2021-10-20 19:34:01 -0400

    updated db URI to work with mongoDb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contact"
+ //   "URI": "mongodb://localhost/business_contact"
+    "URI": "mongodb+srv://Nishit_1:hkTcheZtydzl1FNP@mongodbserver.dkqff.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
